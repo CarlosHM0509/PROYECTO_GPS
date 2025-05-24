7727b16bd84ab41f8091e159812b3415f2e5931d
Carlos Hernandez <chernandezm42@miumg.edu.gt>
2025-05-23 22:51:52 -0600

    prueba1

--- src/main/java/MODELO/Main.java
+++ src/main/java/MODELO/Main.java
@@ -1,4 +1,19 @@
 package MODELO;
 
-public class MAIN {
+public class Main {
+    public static void main(String[] args) {
+        // Crear ciudades
+        Ciudad guatemala = new Ciudad("Ciudad de Guatemala", 14.6349, -90.5069, 1500);
+        Ciudad antigua = new Ciudad("Antigua Guatemala", 14.5596, -90.7343, 1530);
+
+        // Imprimir ciudades
+        System.out.println("Probando clase Ciudad:");
+        System.out.println(guatemala);
+        System.out.println(antigua);
+
+        // Probar método equals
+        Ciudad copiaGuatemala = new Ciudad("Ciudad de Guatemala", 0, 0, 0);
+        System.out.println("¿Guatemala es igual a su copia? " + guatemala.equals(copiaGuatemala));
+    }
 }
+
